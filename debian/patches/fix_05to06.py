cc44263c0c1f052e7df0d5041320e042accfdd49
Gunnar Wolf <gwolf@debian.org>
2008-04-14 11:28:50 -0500

    Imported Debian patch 0.6.1-1

---
+++ debian/patches/fix_05to06.py
@@ -0,0 +1,18 @@
+Index: cherokee-0.6.1/contrib/05to06.py
+===================================================================
+--- cherokee-0.6.1.orig/contrib/05to06.py	2008-04-14 10:15:42.000000000 -0500
++++ cherokee-0.6.1/contrib/05to06.py	2008-04-14 10:26:02.000000000 -0500
+@@ -270,7 +270,12 @@
+             more = self._process_entry_guts (prefix)
+             if not more: break
+ 
+-        print "%s!priority = %d" % (prefix, self._vserver_last_priority * 10)
++        try:
++            priority = self._vserver_last_priority * 10
++        except AttributeError:
++            self._vserver_last_priority = 1
++            priority = 10
++        print "%s!priority = %d" % (prefix, priority * 10)
+         self._vserver_last_priority += 1
+ 
+     def _process_encoder (self, prefix):
